1098be467f974e6e131e6af7fe0db79b672bc932
Allen Miao <allen.miao@yale.edu>
2018-08-26 18:08:13 -0700

    Add test file, clean up function

--- scripts/output_formatter.py
+++ scripts/output_formatter.py
@@ -1,30 +1,34 @@
-#!/usr/bin/python
-import sys
 import os
 
 with open("input.txt", "r+") as f:
     lines = f.readlines()
+
+    # Create a new file to write messsages to
     with open("output.txt", "r+") as output:
+        # Wipe whatever was previously in this file, if any
         output.truncate()
+
         for line in lines:
             path = os.path.basename(line)
-            type = ""
+            violation_type = ""
             violation_split = []
+
             if ' error:' in path:
-                type = "Error"
+                violation_type = "Error: "
                 violation_split = path.split(' error: ')
             elif ' warning:' in path:
-                type = "Warning"
+                violation_type = "Warning: "
                 violation_split = path.split(' warning: ')
 
             colon_split = violation_split[0].split(':')
             file_name = colon_split[0]
-            line_number = 'line ' + colon_split[1]
-            message = violation_split[1]
+            line_number = 'Line ' + colon_split[1]
+            full_message = violation_split[1]
+            short_message = full_message.split(': ')[1].split(' (')[0]
+            message = violation_type + short_message
 
             comment = '\n'.join([file_name, line_number, message])
-            output.write(comment + '\n')
+            output.write('\n' + comment + '\n')
         output.seek(0)
-        print(output.read(),end="")
-        output.close()
-    f.close()
+        # Uncomment the line below to dump contents of output file
+        #print(output.read(),end="")
