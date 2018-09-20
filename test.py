874811b39d67492cbb836a826508528d39afae83
Sophie Zhou <sophie@buildschool.com>
2018-09-20 00:01:07 -0700

    temp

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-from urlparse import urlparse
-from urlrequest import urlrequest
-
-url = 'https://api.github.com/graphql'
-post_fields = {'query': '{"query": "query { repository(owner:\"octocat\", name:\"Hello-World\") { id } }"}'}
-
-request = Request(url, urlencode(post_fields).encode())
-request.add_header('Authorization', 'bearer 2ac3e0d1786589fba0d90212068536514673ded5')
-json = urlopen(request).read().decode()
-print(json)
+echo "hi"
