48f4f30e6cc7b8ed4a392066ad9c22b3ab8306a7
Sophie Zhou <sophie@buildschool.com>
2018-09-19 23:09:04 -0700

    temp

--- post_comment.sh
+++ post_comment.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   https://api.github.com/graphql \
-  -H 'Authorization: bearer d068968d498fee66aa8599c0f76e151c552c8084' \
+  -H 'Authorization: bearer 3747a4fc143b98169d443329b152e0a9f2daacfa' \
   -H 'Content-Type: application/json' \
   -d '{"query": "mutation { addComment(input:{clientMutationId: \"1234\", subjectId: \"MDExOlB1bGxSZXF1ZXN0MjE2MTUwMzk4\", body: \"Looks good to me again from CircleCI\"}) { clientMutationId commentEdge {node {body      }}}}" }'
