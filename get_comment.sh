0e3f4d7e03c2cef92e6102a52cc266cf2a441b2f
Sophie Zhou <sophie@buildschool.com>
2018-09-20 00:18:42 -0700

    temp

--- get_comment.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST  https://api.github.com/graphql -H "Authorization: bearer ${CIRCLE_API_PROJECT_TOKEN}" -d '{"query": "query { repository(owner:\"octocat\", name:\"Hello-World\") { id } }"}'
